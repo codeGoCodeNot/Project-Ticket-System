1ba566c9d03f36952e05f5f51c8b0eaed8edcb2f
sendosend <johnsenberdin2930@gmail.com>
2025-12-15 12:11:34 +0800

    feat(layout): Add Header component to handle navigation items

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import Link from "next/link";
 import "./globals.css";
 import { LucideKanban } from "lucide-react";
+import Header from "@/components/header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,31 +32,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <nav
-          className="
-        supports-backdrop-blur:bg-background/60
-        fixed left-0 right-0 top-0 z-20
-        flex justify-between 
-        py-2.5 px-5 border-b
-        w-full
-        "
-        >
-          <div>
-            <Button variant="ghost" asChild>
-              <Link href={homePath()}>
-                <LucideKanban />
-                <h1 className="text-lg font-semibold">TicketHub</h1>
-              </Link>
-            </Button>
-          </div>
-          <div>
-            <Button variant="default" asChild>
-              <Link href={ticketsPath()}>
-                <h1>Tickets</h1>
-              </Link>
-            </Button>
-          </div>
-        </nav>
+        <Header />
         <main
           className="
         py-24 px-8 min-h-screen 
---
+++ src/components/header.tsx
@@ -0,0 +1,37 @@
+import { homePath, ticketsPath } from "@/path";
+import { LucideKanban } from "lucide-react";
+import Link from "next/link";
+import { Button } from "./ui/button";
+
+// this is navigation items for layout
+const Header = () => {
+  return (
+    <nav
+      className="
+        supports-backdrop-blur:bg-background/60
+        fixed left-0 right-0 top-0 z-20
+        flex justify-between 
+        py-2.5 px-5 border-b
+        w-full
+        "
+    >
+      <div>
+        <Button variant="ghost" asChild>
+          <Link href={homePath()}>
+            <LucideKanban />
+            <h1 className="text-lg font-semibold">TicketHub</h1>
+          </Link>
+        </Button>
+      </div>
+      <div>
+        <Button variant="default" asChild>
+          <Link href={ticketsPath()}>
+            <h1>Tickets</h1>
+          </Link>
+        </Button>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
