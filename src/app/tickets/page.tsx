2cccc2aee9b1592f4b2e20c05f898c1b61993320
sendosend <johnsenberdin2930@gmail.com>
2025-12-16 03:20:22 +0800

    feat(src/feat/components): add ticket-list component for streaming in TicketsPage
    - Created  file to handle streaming use cases for the TicketsPage.
    - Prepared the component to simulate or support streaming data functionality.

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,19 +1,15 @@
 import Heading from "@/components/heading";
-import getTickets from "@/features/queries/get-tickets";
-import TicketItem from "@/features/ticket/components/ticket-item";
+import TicketList from "@/features/ticket/components/ticket-list";
+import { Suspense } from "react";
 
 // This is a tickets page
-const TicketsPage = async () => {
-  const tickets = await getTickets();
-
+const TicketsPage = () => {
   return (
     <div className="flex-1 flex flex-col gap-y-8">
       <Heading title="Tickets" desc="All your tickets at one place" />
-      <div className="flex-1 flex flex-col items-center gap-y-4 animate-fade-from-top">
-        {tickets.map((ticket) => (
-          <TicketItem ticket={ticket} key={ticket.id} />
-        ))}
-      </div>
+      <Suspense>
+        <TicketList />
+      </Suspense>
     </div>
   );
 };
