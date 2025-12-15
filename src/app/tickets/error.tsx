d149249736eece48a0a50f986ec9fb45819ee5e0
sendosend <johnsenberdin2930@gmail.com>
2025-12-16 04:18:40 +0800

    feat(src/app/tickets): add Error route for TicketsPage and TicketPage
    - Created an  route to handle runtime errors for the TicketsPage.
    - Added support for displaying error boundaries on ticket-related pages.
    - Ensures better user feedback when unexpected issues occur.

---
+++ src/app/tickets/error.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import Placeholder from "@/components/placeholder";
+
+type ErrorProps = { error: Error };
+
+const Error = ({ error }: ErrorProps) => {
+  return (
+    <Placeholder
+      label={error.message || "Something went wrong!"}
+      button={null}
+    />
+  );
+};
+
+export default Error;
