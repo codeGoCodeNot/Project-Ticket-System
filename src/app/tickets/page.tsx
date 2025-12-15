f3d7cf866d15dae1e686f34c29f9708556fd123e
sendosend <johnsenberdin2930@gmail.com>
2025-12-15 08:59:43 +0800

    designing in tailwind css and installing clsx for conditional styling

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,5 +1,6 @@
 import { initialTickets } from "@/data";
 import { ticketPath } from "@/path";
+import clsx from "clsx";
 import Link from "next/link";
 
 const TICKET_ICONS = {
@@ -11,18 +12,35 @@ const TICKET_ICONS = {
 // This is a tickets page
 const TicketsPage = () => {
   return (
-    <div>
-      <h2>
+    <div className="flex-1 flex flex-col gap-y-8">
+      <div>
+        <h2 className="text-3xl font-bold tracking-tight">Tickets Page</h2>
+        <p className="text-sm text-muted-foreground">
+          All your tickets at one place
+        </p>
+      </div>
+
+      <div className="flex-1 flex flex-col items-center gap-y-4">
         {initialTickets.map((ticket) => (
-          <div key={ticket.id}>
+          <div
+            key={ticket.id}
+            className="w-full max-w-[420px] p-4 border border-slate-100 rounded"
+          >
             <div>{TICKET_ICONS[ticket.status]}</div>
-            <h2 className="text-lg">{ticket.title}</h2>
+            <h3 className="text-lg">{ticket.title}</h3>
+            <p
+              className={clsx("text-sm text-slate-500 truncate", {
+                "line-through": ticket.status === "DONE",
+              })}
+            >
+              {ticket.content}
+            </p>
             <Link href={ticketPath(ticket.id)} className="text-sm underline">
               View
             </Link>
           </div>
         ))}
-      </h2>
+      </div>
     </div>
   );
 };
