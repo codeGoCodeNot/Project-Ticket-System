f2e9ec840955ac9b1dd7ffb7f97b547b980fa271
sendosend <johnsenberdin2930@gmail.com>
2025-12-16 01:57:57 +0800

    feat(app/tickets/[ticketid]): convert to server components for data fetching
    - Updated the ticket details page to use server components for data fetching.
    - Removed client-side fetching logic to improve performance and leverage server-rendered data.

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,23 +1,10 @@
-"use client";
-
 import Heading from "@/components/heading";
 import getTickets from "@/features/queries/get-tickets";
 import TicketItem from "@/features/ticket/components/ticket-item";
-import { Ticket } from "@/features/ticket/types";
-import { useEffect, useState } from "react";
 
 // This is a tickets page
-const TicketsPage = () => {
-  const [tickets, setTickets] = useState<Ticket[]>([]);
-
-  useEffect(() => {
-    const fetchTickets = async () => {
-      const result = await getTickets();
-      setTickets(result);
-    };
-
-    fetchTickets();
-  }, []);
+const TicketsPage = async () => {
+  const tickets = await getTickets();
 
   return (
     <div className="flex-1 flex flex-col gap-y-8">
