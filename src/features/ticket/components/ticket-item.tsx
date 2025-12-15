3f07779782237ab9224aaf16cfaf269c357523e5
sendosend <johnsenberdin2930@gmail.com>
2025-12-15 13:31:45 +0800

    feat(features/ticket): Modify TicketItem component , added detailButton and fix max-w

--- src/features/ticket/components/ticket-item.tsx
+++ src/features/ticket/components/ticket-item.tsx
@@ -1,38 +1,40 @@
-import {
-  Card,
-  CardHeader,
-  CardTitle,
-  CardContent,
-  CardFooter,
-} from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { ticketPath } from "@/path";
 import Link from "next/link";
 import { TICKET_ICONS } from "../constant";
 import { Ticket } from "../types";
+import { LucideSquareArrowOutUpRight } from "lucide-react";
+import { Button } from "@/components/ui/button";
 
 type TicketItemProps = {
   ticket: Ticket;
 };
 
 const TicketItem = ({ ticket }: TicketItemProps) => {
-  return (
-    <Card className="w-full max-w-[420px]">
-      <CardHeader>
-        <CardTitle className="flex gap-x-2 items-center">
-          <span>{TICKET_ICONS[ticket.status]}</span>
-          <span>{ticket.title}</span>
-        </CardTitle>
-      </CardHeader>
+  const detailButton = (
+    <Button asChild variant="outline" size="icon">
+      <Link href={ticketPath(ticket.id)}>
+        <LucideSquareArrowOutUpRight className="h-4 w-4" />
+      </Link>
+    </Button>
+  );
 
-      <CardContent>
-        <span className="line-clamp-3">{ticket.content}</span>
-      </CardContent>
-      <CardFooter>
-        <Link href={ticketPath(ticket.id)} className="text-sm underline">
-          View
-        </Link>
-      </CardFooter>
-    </Card>
+  return (
+    <div className="w-full max-w-[420px] flex gap-x-1">
+      <Card className="w-full ">
+        <CardHeader>
+          <CardTitle className="flex gap-x-2 items-center">
+            <span>{TICKET_ICONS[ticket.status]}</span>
+            <span>{ticket.title}</span>
+          </CardTitle>
+        </CardHeader>
+
+        <CardContent>
+          <span className="line-clamp-3">{ticket.content}</span>
+        </CardContent>
+      </Card>
+      <div className="flex flex-col gap-y-1">{detailButton}</div>
+    </div>
   );
 };
 
