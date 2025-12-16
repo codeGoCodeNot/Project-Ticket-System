bf26d70692d049690444fed414d2c2f006584ec1
sendosend <johnsenberdin2930@gmail.com>
2025-12-16 11:40:03 +0800

    feat: Update Prisma seeding script to clean tickets with deleteMany

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -12,20 +12,17 @@ const prisma = new PrismaClient({
 
 const tickets = [
   {
-    id: "1",
     title: "Ticket 1",
     content: "This is the first ticket from the database.",
     status: "DONE" as const,
   },
 
   {
-    id: "2",
     title: "Ticket 2",
     content: "This is the second ticket from the database.",
     status: "OPEN" as const,
   },
   {
-    id: "3",
     title: "Ticket 3",
     content: "This is the third ticket from the database.",
     status: "IN_PROGRESS" as const,
@@ -33,6 +30,8 @@ const tickets = [
 ];
 
 const seed = async () => {
+  await prisma.ticket.deleteMany();
+
   await prisma.ticket.createMany({
     data: tickets,
   });
