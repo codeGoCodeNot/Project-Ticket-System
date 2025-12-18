88a1643edc93dcd39cb1c40bed2d186176e831b9
sendosend <johnsenberdin2930@gmail.com>
2025-12-19 05:30:57 +0800

    feat: added useTransition for low-priority updates in TicketUpsertForm

--- src/features/ticket/components/ticket-upsert-form.tsx
+++ src/features/ticket/components/ticket-upsert-form.tsx
@@ -1,18 +1,30 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import { Label } from "@radix-ui/react-label";
 import { Ticket } from "../../../../generated/prisma/client";
 import upsertTicket from "@/features/actions/upsert-ticket";
+import { useTransition } from "react";
+import { LucideLoaderCircle } from "lucide-react";
 
 type TicketUpsertFormProps = {
   ticket?: Ticket;
 };
 
 const TicketUpsertForm = ({ ticket }: TicketUpsertFormProps) => {
+  const [isPending, startTransition] = useTransition();
+
+  const upsertTicketAction = (formData: FormData) => {
+    startTransition(async () => {
+      await upsertTicket.bind(null, ticket?.id)(formData);
+    });
+  };
+
   return (
     <form
-      action={upsertTicket.bind(null, ticket?.id)}
+      action={upsertTicketAction}
       className="
     flex flex-col gap-y-2
     "
@@ -23,7 +35,10 @@ const TicketUpsertForm = ({ ticket }: TicketUpsertFormProps) => {
       <Label htmlFor="content">Content</Label>
       <Textarea id="content" name="content" defaultValue={ticket?.content} />
 
-      <Button type="submit">{ticket ? "Edit" : "Create"}</Button>
+      <Button disabled={isPending} type="submit">
+        {isPending && <LucideLoaderCircle className="h-4 w-4 animate-spin" />}
+        {ticket ? "Edit" : "Create"}
+      </Button>
     </form>
   );
 };
