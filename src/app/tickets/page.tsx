8d2d4e77c5553afa0d119f4d953ce2408c789ab9
sendosend <johnsenberdin2930@gmail.com>
2025-12-18 15:01:32 +0800

    feat(components): add CardCompact component for configurable and reusable card layouts

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,12 +1,6 @@
+import CardCompact from "@/components/card-compact";
 import Heading from "@/components/heading";
 import Spinner from "@/components/spinner";
-import {
-  Card,
-  CardHeader,
-  CardTitle,
-  CardDescription,
-  CardContent,
-} from "@/components/ui/card";
 import TicketList from "@/features/ticket/components/ticket-list";
 import TicketCreateForm from "@/features/ticket/components/ticketCreateForm";
 import { Suspense } from "react";
@@ -17,15 +11,12 @@ const TicketsPage = () => {
     <div className="flex-1 flex flex-col gap-y-8">
       <Heading title="Tickets" desc="All your tickets at one place" />
 
-      <Card className="w-full max-w-[420px] self-center">
-        <CardHeader className="flex flex-col gap-y-2">
-          <CardTitle>Create Ticket</CardTitle>
-          <CardDescription>A new ticket will be created</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <TicketCreateForm />
-        </CardContent>
-      </Card>
+      <CardCompact
+        title="Create Ticket"
+        desc="A new ticket will be created"
+        classname="flex flex-col gap-y-2"
+        content={<TicketCreateForm />}
+      />
 
       <Suspense fallback={<Spinner />}>
         <TicketList />
---
+++ src/components/card-compact.tsx
@@ -0,0 +1,37 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "./ui/card";
+
+type CardCompactProps = {
+  title: string;
+  desc: string;
+  content: React.ReactElement;
+  footer?: React.ReactElement;
+  classname?: string;
+};
+
+const CardCompact = ({
+  title,
+  desc,
+  content,
+  footer,
+  classname,
+}: CardCompactProps) => {
+  return (
+    <Card className="w-full max-w-[420px] self-center">
+      <CardHeader className="flex flex-col gap-y-2">
+        <CardTitle>{title}</CardTitle>
+        <CardDescription>{desc}</CardDescription>
+      </CardHeader>
+      <CardContent>{content}</CardContent>
+      {footer && <CardFooter>{footer}</CardFooter>}
+    </Card>
+  );
+};
+
+export default CardCompact;
