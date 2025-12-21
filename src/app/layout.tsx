c17171b8966d116a2b1dad12a6ab236ffc54417d
codeGoCodeNot <johnsenberdin2930@gmail.com>
2025-12-22 05:39:49 +0800

    feat: Move RedirectToast to Template component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,7 +43,6 @@ export default function RootLayout({
             {children}
           </main>
           <Toaster expand />
-          <RedirectToast />
         </ThemeProvider>
       </body>
     </html>
