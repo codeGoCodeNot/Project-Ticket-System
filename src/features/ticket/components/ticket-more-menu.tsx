05e0b4bcb1ab2be896ff16656793a420a4eb0f42
codeGoCodeNot <johnsenberdin2930@gmail.com>
2025-12-22 15:09:47 +0800

    feat: Add dropdown menu for managing ticket status and actions

--- src/features/ticket/components/ticket-more-menu.tsx
+++ src/features/ticket/components/ticket-more-menu.tsx
@@ -2,10 +2,14 @@ import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { LucideTrash } from "lucide-react";
-import { Ticket } from "../../../../generated/prisma/client";
+import { Ticket, TicketStatus } from "../../../../generated/prisma/client";
+import { TICKET_STATUS_LABELS } from "../constant";
 
 type TicketMoreMenuProps = {
   ticket: Ticket;
@@ -15,15 +19,27 @@ type TicketMoreMenuProps = {
 const TicketMoreMenu = ({ ticket, trigger }: TicketMoreMenuProps) => {
   const deleteButton = (
     <DropdownMenuItem>
-      <LucideTrash className="h-4 w-4 mr-2" />
+      <LucideTrash className="h-4 w-4" />
       <span>Delete</span>
     </DropdownMenuItem>
+  );
+
+  const ticketStatusRadioGroupItems = (
+    <DropdownMenuRadioGroup value={ticket.status}>
+      {(Object.keys(TICKET_STATUS_LABELS) as TicketStatus[]).map((key) => (
+        <DropdownMenuRadioItem value={key} key={key}>
+          {TICKET_STATUS_LABELS[key]}
+        </DropdownMenuRadioItem>
+      ))}
+    </DropdownMenuRadioGroup>
   );
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>{trigger}</DropdownMenuTrigger>
       <DropdownMenuContent className="w-56" side="right">
+        {ticketStatusRadioGroupItems}
+        <DropdownMenuSeparator />
         {deleteButton}
       </DropdownMenuContent>
     </DropdownMenu>
