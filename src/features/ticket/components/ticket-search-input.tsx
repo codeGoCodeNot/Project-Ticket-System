237194b2741807d0968c06a595a1e3c192916804
CodeGoCodeNot <johnsenberdin2930@gmail.com>
2026-01-29 07:08:55 +0800

    fix: reset pagination to first page when performing a new search

--- src/features/ticket/components/ticket-search-input.tsx
+++ src/features/ticket/components/ticket-search-input.tsx
@@ -1,7 +1,11 @@
 "use client";
 
-import { useQueryState } from "nuqs";
-import { searchParser } from "../search-params";
+import { useQueryState, useQueryStates } from "nuqs";
+import {
+  paginationOptions,
+  paginationParser,
+  searchParser,
+} from "../search-params";
 import SearchInput from "@/components/search-input";
 
 type TicketSearchInputProps = {
@@ -10,11 +14,20 @@ type TicketSearchInputProps = {
 
 const TicketSearchInputProps = ({ placeholder }: TicketSearchInputProps) => {
   const [search, setSearch] = useQueryState("search", searchParser);
+  const [pagination, setPagination] = useQueryStates(
+    paginationParser,
+    paginationOptions,
+  );
+
+  const handleSearch = (value: string) => {
+    setPagination({ ...pagination, page: 0 });
+    setSearch(value);
+  };
 
   return (
     <SearchInput
       placeholder={placeholder}
-      onChange={setSearch}
+      onChange={handleSearch}
       value={search}
     />
   );
