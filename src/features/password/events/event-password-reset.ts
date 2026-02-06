69e57df95b3b96d8116020655449b7669d085285
codeGoCodeNot <johnsenberdin2930@gmail.com>
2026-02-06 11:24:04 +0800

    feat: add error handling to password reset event

--- src/features/password/events/event-password-reset.ts
+++ src/features/password/events/event-password-reset.ts
@@ -26,6 +26,9 @@ export const passwordResetEvent = inngest.createFunction(
       passwordResetLink,
     );
 
+    if (result.error)
+      throw new Error(`${result.error.name}: ${result.error.message}`);
+
     return { event, body: result };
   },
 );
