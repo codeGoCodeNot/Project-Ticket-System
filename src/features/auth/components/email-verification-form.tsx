426054aef7f4e45e2dfe9e3390e13c43c62b39c4
codeGoCodeNot <johnsenberdin2930@gmail.com>
2026-02-07 14:55:36 +0800

    feat: add InputOTP with letter-only pattern for email verification

--- src/features/auth/components/email-verification-form.tsx
+++ src/features/auth/components/email-verification-form.tsx
@@ -4,9 +4,14 @@ import FieldError from "@/components/form/field-error";
 import Form from "@/components/form/form";
 import SubmitButton from "@/components/form/submit-button";
 import { EMPTY_ACTION_STATE } from "@/components/form/utils/to-action-state";
-import { Input } from "@/components/ui/input";
+import {
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSlot,
+} from "@/components/ui/input-otp";
 import { useActionState } from "react";
 import emailVerification from "../actions/email-verification";
+import { REGEXP_ONLY_CHARS } from "input-otp";
 
 const EmailVerificationForm = () => {
   const [actionState, action] = useActionState(
@@ -16,11 +21,19 @@ const EmailVerificationForm = () => {
 
   return (
     <Form actionState={actionState} action={action}>
-      <Input
-        name="code"
-        placeholder="Verification Code"
+      <InputOTP
+        maxLength={8}
+        pattern={REGEXP_ONLY_CHARS}
         defaultValue={actionState.payload?.get("code") as string}
-      />
+        name="code"
+      >
+        <InputOTPGroup className="flex justify-center w-full">
+          {[...Array(8)].map((_, index) => (
+            <InputOTPSlot key={index} index={index} />
+          ))}
+        </InputOTPGroup>
+      </InputOTP>
+
       <FieldError actionState={actionState} name="code" />
 
       <SubmitButton label="Verify Email" />
